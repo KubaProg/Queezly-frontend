0eee00af1630c761574ebaa8ba59901e00fa1243
Jakub_Opielka <jakub.opielka@q-perior.com>
2023-10-10 20:06:11 +0200

    Homepage default layou

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CourseCockpitComponent } from './home-page/course-cockpit/course-cockpit.component';
+import { CoursesListComponent } from './home-page/courses-list/courses-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    CourseCockpitComponent,
+    CoursesListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/home-page/course-cockpit/course-cockpit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-cockpit',
+  templateUrl: './course-cockpit.component.html',
+  styleUrls: ['./course-cockpit.component.css']
+})
+export class CourseCockpitComponent {
+
+}
