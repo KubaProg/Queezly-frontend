288e13233951316f96bf65bbaf79dc100aac2e52
Jakub_Opielka <jakub.opielka@q-perior.com>
2023-10-11 19:36:46 +0200

    Refactored naming from course to quiz

---
+++ src/app/home-page/courses-list/quiz-tile/quiz-tile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quizquiz-tile',
+  templateUrl: './quiz-tile.component.html',
+  styleUrls: ['./quiz-tile.component.css']
+})
+export class QuizTileComponent {
+
+}
