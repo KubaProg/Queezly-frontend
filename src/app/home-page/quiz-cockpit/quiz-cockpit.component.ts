f1a297501a50342ac84795b67f60fd13c5951303
Jakub_Opielka <jakub.opielka@q-perior.com>
2023-10-19 19:45:41 +0200

    cleaning search criteria working

--- src/app/home-page/quiz-cockpit/quiz-cockpit.component.ts
+++ src/app/home-page/quiz-cockpit/quiz-cockpit.component.ts
@@ -21,14 +21,23 @@ export class QuizCockpitComponent {
   constructor(private apiService: ApiService) {}
 
   onSearchQuizes(selectedCategory: string, selectedLevel: string, quizName: string) {
-    this.parameters = new QuizParameters(quizName,selectedCategory,selectedLevel)
+    // Check if selectedCategory and selectedLevel are empty, and if so, set them to undefined
+    if (selectedCategory === '') {
+      selectedCategory = undefined;
+    }
+    if (selectedLevel === '') {
+      selectedLevel = undefined;
+    }
+
+    this.parameters = new QuizParameters(quizName, selectedCategory, selectedLevel);
     this.apiService.getQuizzesByRequirements(this.parameters).subscribe(
       (response) => {
         this.searchedQuizzes = response;
         this.apiService.updatedQuizzes.emit(response);
       }
-    )
+    );
   }
+
 
 
 }
